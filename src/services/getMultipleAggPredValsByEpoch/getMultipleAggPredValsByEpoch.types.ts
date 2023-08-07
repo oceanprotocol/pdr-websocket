c0c62e415441b5e7475e86b0431010f7c2521185
trizin <25263018+trizin@users.noreply.github.com>
2023-08-07 11:45:06 +0300

    Fix

--- src/services/getMultipleAggPredValsByEpoch/getMultipleAggPredValsByEpoch.types.ts
+++ src/services/getMultipleAggPredValsByEpoch/getMultipleAggPredValsByEpoch.types.ts
@@ -2,8 +2,8 @@ import Predictoor from "../../utils/contracts/Predictoor";
 import { initializeAutorization } from "../initializeAuthorization";
 
 export type TGetMultipleAggPredValsByEpochArgs = {
-  currentBlockNumber: number;
-  epochStartBlockNumber: number;
+  currentTs: number;
+  epochStartTs: number;
   secondsPerEpoch: number;
   epochs: Array<number>;
   contracts: Array<Predictoor>;
