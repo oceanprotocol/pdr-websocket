cd197d8f72d4694594cf3cf88f6ac3df873869a0
Norbert <katunanorbert@gmail.com>
2023-08-18 11:41:37 +0300

    update env variables names

--- src/utils/networkProvider.ts
+++ src/utils/networkProvider.ts
@@ -7,7 +7,7 @@ type NetworkConfig = Record<NetworkNames, string>;
 
 // Define your network configuration mapping the env variable to the network URL
 const networkConfig: NetworkConfig = {
-  development: process.env.GANACHE_HOST ? process.env.GANACHE_HOST : 'http://172.15.0.15:8545',
+  development: process.env.GANACHE_URL ? process.env.GANACHE_URL : 'http://172.15.0.15:8545',
   mock: "http://localhost:8545",
   testnet: "",
   mainnet: "",
