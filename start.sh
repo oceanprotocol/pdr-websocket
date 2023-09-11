d570ea0ac29a8725cb0d6fcd3873c0fbcd41fd75
trizin <25263018+trizin@users.noreply.github.com>
2023-09-11 16:52:22 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Node Environment is: $NODE_ENV"
+
 if [ "$NODE_ENV" = "development" ]; then 
   npm run start-dev
-else
-if [ "$NODE_ENV" = "staging" ]; then 
+elif [ "$NODE_ENV" = "staging" ]; then 
   npm run staging
 else 
   npm run start-production
