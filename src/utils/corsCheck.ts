e1220a725b65201c315798a50319b9162d18c693
Norbert <katunanorbert@gmail.com>
2023-10-06 10:56:09 +0300

    add production env variable to cors check

--- src/utils/corsCheck.ts
+++ src/utils/corsCheck.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import cors from "cors";
 
 export const corsCheck = (app: Express) => {
-  if (process.env.ENVIRONMENT === "staging") {
+  if (process.env.ENVIRONMENT === "staging" || process.env.ENVIRONMENT === "production") {
     app.use(
       cors({
         origin: '*'
